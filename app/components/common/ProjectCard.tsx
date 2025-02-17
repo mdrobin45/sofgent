019f6e550f7b31765ae16d48b2a24d77c91b719d
mdrobin45 <robinrana4r@gmail.com>
2025-02-17 13:16:14 +0600

    create project page

--- app/components/common/ProjectCard.tsx
+++ app/components/common/ProjectCard.tsx
@@ -1,16 +1,27 @@
 import cardShape from "@assets/images/about/about-card-shape.webp";
-import dummyImg from "@assets/images/services/service-hero.webp";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function ProjectCard() {
+interface ProjectFieldsType {
+   title: string;
+   thumbnail: string;
+   slug: string;
+   overview: string;
+   publish_date: string;
+   screenshots: { title: string; image: string }[];
+}
+export default function ProjectCard({
+   project,
+}: {
+   project: ProjectFieldsType;
+}) {
    return (
       <div data-aos="fade-left" className="col-span-4">
          <div className="pb-[92px] pr-[30px] w-full bg-white relative">
             <Image
                width={200}
                height={200}
-               src={dummyImg}
+               src={project.thumbnail}
                alt="Project Thumbnail"
                className="w-full max-w-[380px] rounded-lg"
             />
@@ -23,12 +34,12 @@ export default function ProjectCard() {
                   className="absolute right-2 top-bottom-moving"
                />
                <h1 className="transition-all duration-300 text-paragraph group-hover:text-white">
-                  February 25, 2024
+                  {project.publish_date}
                </h1>
                <h2 className="text-18 sm:text-20 font-semibold mt-3 max-w-[255px] group-hover:text-white">
-                  Lessons Learned from an IT Transformation Journey
+                  {project.title}
                </h2>
-               <Link href="/project-details">
+               <Link href={`/projects/${project.slug}`}>
                   <div className="flex items-center gap-2 mt-4 group">
                      <span className="relative font-medium leading-5 text-white border-b border-transparent font-inter before:inline-block before:border-white before:border-b before:absolute before:bottom-0 before:transition-all before:duration-300 before:w-0 hover:before:w-full before:overflow-hidden before:h-5">
                         Read More
